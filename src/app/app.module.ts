e832a57eae581976abdb5f2f7ec3c2d69de99029
islamashraful <rx.ju.edu@gmail.com>
2019-07-17 23:51:13 +0600

    Added Layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './page/home/home.component';
+import { LayoutComponent } from './page/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/page/layout/layout.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * Layout Component
+ * A wrapper component for all of the other page
+ */
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html'
+})
+export class LayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
