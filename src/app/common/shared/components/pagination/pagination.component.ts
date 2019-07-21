d74b97622572365d6629a9b2ffeb10acad84e2d4
islamashraful <rx.ju.edu@gmail.com>
2019-07-22 01:30:42 +0600

    Added minor formatiing and css

--- src/app/common/shared/components/pagination/pagination.component.ts
+++ src/app/common/shared/components/pagination/pagination.component.ts
@@ -5,8 +5,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
  */
 @Component({
   selector: 'app-pagination',
-  templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.scss']
+  templateUrl: './pagination.component.html'
 })
 export class PaginationComponent implements OnInit {
   /**
