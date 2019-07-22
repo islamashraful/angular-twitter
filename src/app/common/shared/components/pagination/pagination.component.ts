ff19444abd30873d6369207f704c575a9989b25a
islamashraful <rx.ju.edu@gmail.com>
2019-07-22 20:41:47 +0600

    Added responsive style on pagination component

--- src/app/common/shared/components/pagination/pagination.component.ts
+++ src/app/common/shared/components/pagination/pagination.component.ts
@@ -5,7 +5,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
  */
 @Component({
   selector: 'app-pagination',
-  templateUrl: './pagination.component.html'
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent implements OnInit {
   /**
